a01de016deec467c17b14d6f80934d8ab4c75b94
sital-tharu <sitatharu769@gmail.com>
2025-10-04 00:33:58 +0530

    feat(book): implement full CRUD operations in BookService

--- src/book/book.service.ts
+++ src/book/book.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateBookInput } from './dto/create_bookinput';
+import { UpdateBookInput } from './dto/update_bookinput';
 
 @Injectable()
 export class BookService {
@@ -8,5 +9,23 @@ export class BookService {
 
     createBook(data:CreateBookInput){
         return this.prisma.book.create({data});
+    }
+
+    findAll(){
+        return this.prisma.book.findMany();
+    }
+
+    findOne(id:string){
+        return this.prisma.book.findUnique({where:{id}});
+    }
+
+    update(data : UpdateBookInput){
+        return this.prisma.book.update({where : {id: String(data.id)},
+             data: {title: data.title,
+                 author: data.author}});
+    }
+
+    remove(id:string){
+        return this.prisma.book.delete({where:{id}});
     }
 }
