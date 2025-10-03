1bccea95aea1bb0de41a35c740824ee8053744f8
sital-tharu <sitatharu769@gmail.com>
2025-10-03 23:59:43 +0530

    feat(book): define UpdateBookInput DTO with partial fields and ID

---
+++ src/book/dto/update_bookinput.ts
@@ -0,0 +1,8 @@
+import { InputType, Field, PartialType } from "@nestjs/graphql";
+import { CreateBookInput } from "./create_bookinput";
+@InputType()
+export class UpdateBookInput extends PartialType(CreateBookInput) {
+    @Field()
+    id: number;
+
+}
