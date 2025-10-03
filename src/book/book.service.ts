9be6f9b853ec2066682d7ec27ea95534176e16e0
sital-tharu <sitatharu769@gmail.com>
2025-10-04 00:25:08 +0530

    feat(book): implement createBook service method using Prisma

--- src/book/book.service.ts
+++ src/book/book.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateBookInput } from './dto/create_bookinput';
 
 @Injectable()
-export class BookService {}
+export class BookService {
+    constructor(private prisma: PrismaService) {}
+
+    createBook(data:CreateBookInput){
+        return this.prisma.book.create({data});
+    }
+}
