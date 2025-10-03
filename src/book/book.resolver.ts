f8a7f06fbe317e57fdbf928ad099b1af926d2864
sital-tharu <sitatharu769@gmail.com>
2025-10-04 00:43:15 +0530

    feat(book): implement GraphQL queries for fetching books

--- src/book/book.resolver.ts
+++ src/book/book.resolver.ts
@@ -1,4 +1,21 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { Book } from './model/book.model';
+import { BookService } from './book.service';
 
-@Resolver()
-export class BookResolver {}
+@Resolver(() => Book)
+export class BookResolver {
+    constructor(private readonly bookService: BookService) { }
+
+    @Query(() => [Book])
+    getAllBooks() {
+        return this.bookService.findAll()
+    }
+
+    @Query(() => Book)
+    getBook(@Args('id') id: string) {
+        return this.bookService.findOne(id)
+    }
+
+
+
+}
