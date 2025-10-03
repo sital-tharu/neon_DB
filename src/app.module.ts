2fcb1dfb2dfd43c84aed1868e477f8ba58287821
sital-tharu <sitatharu769@gmail.com>
2025-10-04 01:07:58 +0530

    fix(app): remove ApolloDriver and switch to legacy GraphQLModule setup for Apollo Server v3

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,17 +4,18 @@ import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { BookModule } from './book/book.module';
 import { GraphQLModule } from '@nestjs/graphql';
-import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { join } from 'path';
 
 @Module({
-  imports: [GraphQLModule.forRoot<ApolloDriverConfig>({
-    driver: ApolloDriver,
-    autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
-    sortSchema: true,
-    playground: true
-
-  }), PrismaModule, BookModule],
+  imports: [
+    GraphQLModule.forRoot({
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      sortSchema: true,
+      playground: true,
+    }),
+    PrismaModule,
+    BookModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
